e48aa1b5ed2f1d46c8f26c1a48b7157536eec54e
ankitmlive <ankitmlive@gmail.com>
2020-07-13 21:38:40 +0530

    added department model

--- department/models.py
+++ department/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+class Department(models.Model):
+    """
+    This table will belongs to department of various loactions
+    """
+    name = models.CharField(max_length=80, blank=False,)
+    location = models.CharField(max_length=150, blank=False,)
+    date_of_innaugration = models.DateField(null=False)
+    created_at  = models.DateTimeField(auto_now_add=True)
+    updated_at  = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ('-created_at',)
