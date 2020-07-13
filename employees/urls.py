3fb67d5b538735fd4ca5381810c2b0888e8b4bdc
ankitmlive <ankitmlive@gmail.com>
2020-07-13 20:42:09 +0530

    added accounts urls

---
+++ employees/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    # Auth url's -- Account related urls
+    path('sign-up/', views.UserSignUpView.as_view(), name='sign-up'),
+    path('sign-in/', views.UserSignInView.as_view(), name='sign-in'),
+    path('sign-out/', views.UserSignOutView.as_view(), name='sign-out'),
+    path('activate-account/', views.UserActivationView.as_view(), name='activate-account'),
+    path('change-password/', views.ChangePasswordView.as_view(), name="change-password"),
+    path('reset-password/', views.ResetPasswordView.as_view(), name="reset-password"),
+    path('reset-password-confirm/', views.ConfirmResetPasswordView.as_view(), name="reset-password-confirm"),
+]
