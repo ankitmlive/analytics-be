d6e7f29dbcbebdb4ba3c77b8c5f35b52bb3100c9
ankitmlive <ankitmlive@gmail.com>
2020-07-13 20:28:10 +0530

    added employee app

--- analytics/urls.py
+++ analytics/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include,path
+
+#import Rest Framework
+from rest_framework import routers
+from accounts.views import UserViewSet
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/', include(router.urls)),
+    path('api/v1/account/', include('employees.urls')),
 ]
